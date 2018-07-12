93ffa6003a16494895d20bc097331e9427245ca2
Michael Falkowski <michael.falkowski@accenture.com>
2018-07-12 13:16:24 -0500

    mo data mo problems

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ import { format, formatDistance, formatRelative, subDays } from 'date-fns'
 const stats = new Stats();
 stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild( stats.dom );
-
+// console.log(animationData);
 
 // Set your mapbox token here
 const MAPBOX_TOKEN = 'pk.eyJ1IjoibWZhbGtvd3NraSIsImEiOiJjamplc241c3U0cWdyM3FvZ2lhbnRpMWNqIn0.IoccaoYEVuGJZjvsADuwAg' // eslint-disable-line
